9bfa06f8a1c01aeeb73aff2b193569b42a44beef
jodoyoung <jodoyoung36@gmail.com>
2018-01-01 20:54:13 +0900

    401 location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
 import { AppRoutingModule } from './routing.module';
 
 import { Mulgore } from './mulgore';
+import { HttpClientConfig } from './common/http.interceptor';
 import { Top } from './top/top';
 import { Bottom } from './bottom/bottom';
 import { Home } from './home/home';
@@ -17,6 +18,9 @@ import { AlbumThumbnail } from './album/album.thumbnail';
   declarations: [
     Mulgore, Top, Bottom, Home, AlbumList, AlbumThumbnail
   ],
+  providers: [ {
+    provide: HTTP_INTERCEPTORS, useClass: HttpClientConfig, multi: true
+  } ],
   bootstrap: [ Mulgore ]
 })
 export class AppModule { }
