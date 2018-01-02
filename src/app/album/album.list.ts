b1e6a33a66c039ab67978799ca856d76df2cf360
jodoyoung <jodoyoung36@gmail.com>
2018-01-02 21:14:19 +0900

    a

--- src/app/album/album.list.ts
+++ src/app/album/album.list.ts
@@ -1,4 +1,4 @@
-import { Component, isDevMode, OnInit } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
@@ -17,19 +17,17 @@ import { AlbumServiceMock } from './album.service.mock';
         deps: [ HttpClient ]
     } ]
 })
-export class AlbumList implements OnInit {
+export class AlbumList {
     private path: string;
     private articles: any[];
     
-    constructor(private route: ActivatedRoute, private albumService: AlbumService) {
-    }
-
-    ngOnInit() {
-        this.path = this.route.snapshot.params['path'];
+    constructor(route: ActivatedRoute, albumService: AlbumService) {
+        this.path = route.snapshot.params['path'];
         if(this.path) {
-            this.articles = this.albumService.getDirectoryItems(this.path);
+            this.articles = albumService.getDirectoryItems(this.path);
         } else {
-            this.articles = this.albumService.getRootDirectoryItems();
+            this.articles = albumService.getRootDirectoryItems();
         }
     }
+
 }
--- src/app/album/album.service.ts
+++ src/app/album/album.service.ts
@@ -6,15 +6,17 @@ export class AlbumService {
     constructor(private http: HttpClient) {}
 
     getRootDirectoryItems(): any[] {
-        let result: any[] = [];
+        let result: any[];
         this.http.get<any[]>('/api/drive').subscribe(data => {
+            console.log("/api/drive - " + data);
             result = data;
         });
+        console.log("/api/drive result - " + result);
         return result;
     }
 
     getDirectoryItems(path: string): any[] {
-        let result: any[] = [];
+        let result: any[];
         this.http.get<any[]>('/api/drive' + path).subscribe(data => {
             result = data;
         });
