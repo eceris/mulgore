45057eac1b52654b0a85e002d78b3ddd129c272f
jodoyoung <jodoyoung36@gmail.com>
2018-01-04 22:44:20 +0900

    design

--- src/app/album/album.service.mock.ts
+++ src/app/album/album.service.mock.ts
@@ -5,22 +5,22 @@ export class AlbumServiceMock extends AlbumService {
 
     getRootDirectoryItems(): Observable<any[]> {
         return Observable.of([
-            {"path":"/SAM_5537.jpg","type":"IMAGE"},
-            {"path":"/test","type":"DIRECTORY"},
-            {"path":"/video","type":"DIRECTORY"}
+            {"path":"/SAM_5537.jpg","type":"IMAGE","name":"SAM_5537.jpg"},
+            {"path":"/test","type":"DIRECTORY","name":"test"},
+            {"path":"/video","type":"DIRECTORY","name":"video"}
         ]);
     }
 
     getDirectoryItems(path: string): Observable<any[]> {
         return Observable.of([
-            {"path":"/test/SAM_5318.jpg","type":"IMAGE"},
-            {"path":"/test/SAM_5526.JPG","type":"IMAGE"},
-            {"path":"/test/SAM_5533.MP4","type":"BINARY"}
+            {"path":"/test/SAM_5318.jpg","type":"IMAGE","name":"SAM_5318.jpg"},
+            {"path":"/test/SAM_5526.JPG","type":"IMAGE","name":"SAM_5526.jpg"},
+            {"path":"/test/SAM_5533.MP4","type":"BINARY","name":"SAM_5533.MP4"}
         ]);
     }
 
     getAlbumDetail(path: string): Observable<any> {
-        return Observable.of({"path":"/test/SAM_5318.jpg","type":"IMAGE"});
+        return Observable.of({"path":"/test/SAM_5318.jpg","type":"IMAGE","name":"SAM_5318.jpg"});
     }
 
 }
