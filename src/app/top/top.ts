0beef8e42ca92fd65a53b19d9c92f932294abf27
jodoyoung <jodoyoung@daou.co.kr>
2018-01-05 14:13:56 +0900

    design, loggedin check

--- src/app/top/top.ts
+++ src/app/top/top.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
     styleUrls: ['./top.css']
 })
 export class Top {
+
     private moveLogin() {
         window.location.href = '/api/auth/login';
+    }
+
+    private isLoggedIn() {
+        let isLoggedIn = false;
+        let cookies = document.cookie.split(';');
+        cookies.forEach(cookie => {
+            if(cookie.startsWith("KEY_AUTH_COOKIE=")) {
+                isLoggedIn = true;
+            }
+        });
+        return isLoggedIn;
     }
 }
